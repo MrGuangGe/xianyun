2d2557d0c3f59c817ce8caff7a4f405cf187ca14
Mr <ninhao1002@163.com>
2019-07-16 17:46:26 +0800

    处理间隔时间出现负数的情况

--- components/air/flightsItem.vue
+++ components/air/flightsItem.vue
@@ -14,7 +14,7 @@
               <span>{{data.org_airport_name}}{{data.org_airport_quay}}</span>
             </el-col>
             <el-col :span="8" class="flight-time">
-              <span>2时20分</span>
+              <span>{{distanceOfTime}}</span>
             </el-col>
             <el-col :span="8" class="flight-airport">
               <strong>{{data.arr_time}}</strong>
@@ -61,6 +61,7 @@
 export default {
   data() {
     return {
+      // 低价推荐的显示与隐藏
       isShow: false
     };
   },
@@ -72,6 +73,32 @@ export default {
       type: Object,
       // 声明属性的默认值
       default: {}
+    }
+  },
+  computed: {
+    // 计算出发时间与到达世间的时间间隔
+    distanceOfTime() {
+      // console.log(this.data);
+      // 20:30  ---  22:50
+      // 通过冒号将这样的时间22:50切割成字符串
+      const start = this.data.dep_time.split(":"); // 出发时间
+      const end = this.data.arr_time.split(":"); // 到达世间
+
+      // 计算间隔多少分钟
+      // + +end是为了隐式转换 否则字符串就会相连了
+      let arrTime = end[0] * 60 + +end[1];
+      let depTime = start[0] * 60 + +start[1];
+
+      // 第二天到达的话需要加上24小时 否则会出现负数
+      if (arrTime < depTime) {
+        arrTime += 24 * 60;
+      }
+
+      const distance = arrTime - depTime;
+      const hour = Math.floor(distance / 60);
+      const min = distance % 60;
+      // computed属性会return回一个字符串
+      return `${hour}时${min}分`;
     }
   }
 };
